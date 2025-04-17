5ef8f31eecdedb883b448d6accd72452859fae05
Iulian Dragan <iulian_dragan@yahoo.com>
2025-04-17 12:52:08 +0200

    ...

--- R/makeOneFunction.R
+++ R/makeOneFunction.R
@@ -8,7 +8,7 @@ func <- list()
     if(is.null(assignVar)){
       assignVar <- 'newObj'
     }
-    func$client <- paste0("function(", assignVar, ", async, datasources, ...)\n ")
+    func$client <- paste0("function(", assignVar, ", async = TRUE, datasources = NULL, ...)\n ")
   } else if(funcType == 'aggregate'){
     func$client <- "function(async, datasources, ...)\n "
   } else {
@@ -18,9 +18,12 @@ func <- list()
 
   func$client <- paste0(func$client, "if(is.null(datasources)){\n  datasources <- datashield.connections_find()\n }\n argList <- list(...)\n ") #arglist <- .encode.arg(list(...), serialize.it = TRUE)\n ")
   func$client <- paste0(func$client, "expr <- c(as.symbol('",funcName, serverSuffix, "'), sapply(names(argList), function(x){\n  ")
-  printSyms <- paste(symbols, collapse = "', '")
-  func$client <- paste0(func$client, "if(x %in% c('", printSyms, "')){\n   return(as.symbol(argList[[x]]))\n  } else {\n   return(.encode.arg(argList[[x]], serialize.it = TRUE))\n  }\n }, simplify = FALSE))\n ")
-  # func <- paste0(func, "myCall <- paste0('",funcName, suffix, "(\"',arglist,'\")')\n ")
+  if(length(symbols > 0 )){
+    printSyms <- paste(symbols, collapse = "', '")
+    func$client <- paste0(func$client, "if(x %in% c('", printSyms, "')){\n   return(as.symbol(argList[[x]]))\n  }\n  ")
+  }
+  func$client <- paste0(func$client, "return(.encode.arg(argList[[x]], serialize.it = TRUE))\n }, simplify = FALSE))\n ")
+
   if(funcType == 'assign' ){
     func$client <- paste0(func$client, "datashield.assign(datasources, ", assignVar, ", as.call(expr), async)\n}")
   } else { # aggregate
@@ -30,7 +33,12 @@ func <- list()
   ### server
   func$server <- "function(...)\n argList <- list(...)\n "
   func$server <- paste0(func$server, "argList <- sapply(names(argList), function(x){\n  ")
-  func$server <- paste0(func$server, "if(x %in% c('", printSyms, "')){\n   return(argList[[x]])\n  } else {\n   return(.decode.arg(argList[[x]]))\n  }\n }, simplify = FALSE)\n ")
+  if(length(symbols > 0 )){
+    printSyms <- paste(symbols, collapse = "', '")
+    func$server <- paste0(func$server, "if(x %in% c('", printSyms, "')){\n   return(as.symbol(argList[[x]]))\n  }\n  ")
+  }
+  func$server <- paste0(func$server, "return(.decode.arg(argList[[x]]))\n }, simplify = FALSE))\n ")
+  #  func$server <- paste0(func$server, "if(x %in% c('", printSyms, "')){\n   return(argList[[x]])\n  } else {\n   return(.decode.arg(argList[[x]]))\n  }\n }, simplify = FALSE)\n ")
   func$server <- paste0(func$server, 'out <- do.call(', package,'::', funcName, ', argList)\n ')
   func$server <- paste0(func$server, 'return(out)\n}')
 
--- R/makePackage.R
+++ R/makePackage.R
@@ -1,26 +1,17 @@
 
-makePackage <- function(packageName, sourceList = list(),
+makePackage <- function(packageName, sourceList = list(), symbols = list(), clientPrefix = 'ds.', serverSuffix = 'DS',
                         authors = 'person("Iulian", "Dragan", email = "iulian.dragan@sib.swiss", role = c("aut", "cre"))',
                         license = NULL, destPath = '.'){
   # restart every time:
   unlink(paste0(tempdir(), '/', packageName), recursive = TRUE)
   myDir <- tempdir()
-  sapply(names(sourceList), function(packName){
+  funcList <- sapply(names(sourceList), function(packName){
    sapply(c('assign', 'aggregate'), function(funType){
      sapply(sourceList[[packName]][[funType]], function(funName){
-       clientFunc <- makeOneClientFunction(funName, funType, )
-
+       makeOneFunction(packageName, funName, funType, 'DS', symbols[[funName]])
      })
    })
  })
 
- sapply(names(srclist), function (packName){
-   sapply(srclist[[packName]], function(funcName){
-   clientFunc <- makeOneClientFunction()
-
-
-
-   })
- })
 
 }
