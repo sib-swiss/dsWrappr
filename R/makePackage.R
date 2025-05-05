ef4abd692cf3a7f41693d2e89603fc76e0344677
Iulian Dragan <iulian_dragan@yahoo.com>
2025-05-05 14:18:30 +0200

    ...

--- R/makePackage.R
+++ R/makePackage.R
@@ -1,17 +1,20 @@
 
-makePackage <- function(packageName, sourceList = list(), symbols = list(), clientPrefix = 'ds.', serverSuffix = 'DS',
+makePackage <- function(packageName, assignList = list(), aggregateList = list(), symbols = list(), clientPrefix = 'ds.', serverSuffix = 'DS',
                         authors = 'person("Iulian", "Dragan", email = "iulian.dragan@sib.swiss", role = c("aut", "cre"))',
                         license = NULL, destPath = '.'){
   # restart every time:
   unlink(paste0(tempdir(), '/', packageName), recursive = TRUE)
   myDir <- tempdir()
-  funcList <- sapply(names(sourceList), function(packName){
-   sapply(names(sourceList[[packName]]), function(funType){
-     sapply(sourceList[[packName]][[funType]], function(funName){
-       makeOneFunction(packageName, funName, funType, 'DS', symbols[[funName]])
+  assignFuncList <- sapply(names(assignList), function(packName){
+     sapply(assignList[[packName]], function(funName){
+       makeOneFunction(packName, funName, 'assign', 'DS', symbols[[funName]])
      })
    })
- })
-
+  aggregateFuncList <- sapply(names(aggregateList), function(packName){
+    sapply(aggregateList[[packName]], function(funName){
+      makeOneFunction(packName, funName, 'assign', 'DS', symbols[[funName]])
+    })
+  })
+  c(assignFuncList, aggregateFuncList)
 
 }
