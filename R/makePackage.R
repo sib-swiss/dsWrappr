b6588755b4e50f5eae08ef82f0d9ff10a1d2b9fe
Iulian Dragan <iulian_dragan@yahoo.com>
2025-05-08 13:51:30 +0200

    write files

--- R/makePackage.R
+++ R/makePackage.R
@@ -3,30 +3,42 @@ makePackage <- function(packageName, assignList = list(), aggregateList = list()
                         authors = 'person("Iulian", "Dragan", email = "iulian.dragan@sib.swiss", role = c("aut", "cre"))',
                         license = NULL, destPath = '.'){
   # restart every time:
+  clientPackageName <- paste0(packageName, 'Client')
   unlink(paste0(tempdir(), '/', packageName), recursive = TRUE)
-  myDir <- tempdir()
+  unlink(paste0(tempdir(), '/', clientPackageName), recursive = TRUE)
+  serverDir <- paste0(tempdir(), '/', packageName)
+  clientDir <- paste0(tempdir(), '/', clientPackageName)
+  dir.create(serverDir)
+  dir.create(clientDir)
   assignFuncList <- lapply(names(assignList), function(packName){
      sapply(assignList[[packName]], function(funName){
       ret <- makeOneFunction(packName, funName, 'assign', 'DS', symbols[[funName]])
-      ret$client <- paste0(clientPrefix, funName,' <- ', ret$client)
-      ret$server <- paste0( funName, serverSuffix, ' <- ', ret$server)
-      return(ret)
-     }, simplify = FALSE)
+      clientFun <- paste0(clientPrefix, funName)
+      serverFun <- paste0(funName, serverSuffix)
+      clientFile <- paste0(clientDir,'/',clientFun, '.R')
+      serverFile <- paste0(serverDir,'/',serverFun, '.R')
+      cat(paste0(clientFun,' <- ', ret$client), file = clientFile)
+      cat(paste0( serverFun, ' <- ', ret$server), file = serverFile)
+     })
    })
   aggregateFuncList <- lapply(names(aggregateList), function(packName){
     sapply(aggregateList[[packName]], function(funName){
       ret <-makeOneFunction(packName, funName, 'aggregate', 'DS', symbols[[funName]])
-      ret$client <- paste0(clientPrefix, funName,' <- ', ret$client)
-      ret$server <- paste0( funName, serverSuffix, ' <- ', ret$server)
-      return(ret)
+      clientFun <- paste0(clientPrefix, funName)
+      serverFun <- paste0(funName, serverSuffix)
+      clientFile <- paste0(clientDir,'/',clientFun, '.R')
+      serverFile <- paste0(serverDir,'/',serverFun, '.R')
+      cat(paste0(clientFun,' <- ', ret$client), file = clientFile)
+      cat(paste0( serverFun, ' <- ', ret$server), file = serverFile)
     })
-  }, simplify = FALSE)
-  c(unlist(assignFuncList, recursive = FALSE), unlist(aggregateFuncList, recursive = FALSE),
-    sapply(c('.encode.arg', '.decode.arg' ), function(fname){
+  })
+
+  Map(function(fname,dest){
       fsource <- capture.output(print(get(fname, envir = as.environment('package:dsWrapR'))))
       fsource[1] <- paste0(fname, ' <- ',fsource[1])
-    # without the lines starting with "<" (meta package rubbish)
-     # cat(fsource[grep('^<', fsource, invert = TRUE)], file = paste0(myDir,'/', packageName,'/R/',fname,'.R'), sep ="\n")
-      paste(fsource[grep('^<', fsource, invert = TRUE)], collapse = "\n")
-    }, simplify = FALSE))
+      # without the lines starting with "<" (meta package rubbish)
+      cat(fsource[grep('^<', fsource, invert = TRUE)], file = paste0(dest,'/little_helpers.R'), sep ="\n")
+      #paste(fsource[grep('^<', fsource, invert = TRUE)], collapse = "\n")
+    }, c('.encode.arg', '.decode.arg'), c(clientDir, serverDir))
+
 }
