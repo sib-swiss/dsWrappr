e1ba24eb8df61ecf7fcb7d82c813990a1491973b
Iulian Dragan <iulian_dragan@yahoo.com>
2025-05-21 15:58:18 +0200

    ...

--- tests/testthat/setup-init.R
+++ tests/testthat/setup-init.R
@@ -1,9 +1,2 @@
 library(DSLite)
-dslite.server1 <- newDSLiteServer(config = defaultDSConfiguration(include=c('dsMissForest')))
-builder <- newDSLoginBuilder()
-builder$append(server="server1", url='dslite.server1',driver = "DSLiteDriver")
-
-logindata <- builder$build()
-opals <- datashield.login(logins = logindata)
-session1 <- dslite.server1$getSession(dslite.server1$getSessionIds())
-data('iris', envir = session1)
+dPath <<-  paste0(tempdir(TRUE), '/test_dsWrapper')
--- tests/testthat/test-makePackage.R
+++ tests/testthat/test-makePackage.R
@@ -1,10 +1,10 @@
 test_that("I can create and load dsMissForest/dsMissForestClient", {
-  dPath <-  paste0(tempdir(TRUE), '/test_dsWrapper')
+  #dPath <-  paste0(tempdir(TRUE), '/test_dsWrapper')
   unlink(dPath, recursive = TRUE)
   dir.create(dPath)
   makePackage('dsMissForest', assignList = list(missForest = c('missForest', 'prodNA')),
               aggregateList = list(missForest = c('mixError', 'nrmse')),
-              symbols = list('ximp', 'xmiss', 'xtrue', 'x'),
+              symbols = list('ximp', 'xmis', 'xtrue', 'x'),
               authors = 'person("Iulian", "Dragan", email = "iulian.dragan@sib.swiss", role = c("aut", "cre"))',
               license = 'GPLv3', destPath = dPath)
   devtools::load_all(paste0(dPath, '/dsMissForestClient'))
@@ -15,7 +15,7 @@ test_that("I can create and load dsMissForest/dsMissForestClient", {
 })
 
 test_that("I can build dsMissForest/dsMissForestClient", {
-  dPath <-  paste0(tempdir(TRUE), '/test_dsWrapper')
+
   s <- devtools::build(paste0(dPath, '/dsMissForest'))
   cl <- devtools::build(paste0(dPath, '/dsMissForestClient'))
   expect_equal(c(s,cl), c(paste0(dPath, '/dsMissForest_0.1.tar.gz'), paste0(dPath, '/dsMissForestClient_0.1.tar.gz')))
@@ -23,8 +23,19 @@ test_that("I can build dsMissForest/dsMissForestClient", {
 })
 
 test_that("I can run functions from dsMissForest in a dsLite environment", {
+  install.packages(paste0(dPath, '/dsMissForest_0.1.tar.gz'))
 
-  ds.prodNA('iris_na', TRUE, NULL, x ='iris' )
-  expect_false(all(complete.cases(session1$iris_na)))
+  dslite.server1 <<- newDSLiteServer(config = defaultDSConfiguration(include=c('dsBase', 'dsMissForest')))
+  builder <- newDSLoginBuilder()
+  builder$append(server="server1", url='dslite.server1',driver = "DSLiteDriver")
+
+  logindata <- builder$build()
+  opals <<- datashield.login(logins = logindata)
+  session1 <- dslite.server1$getSession(dslite.server1$getSessionIds())
+  data('iris', envir = session1)
+  ds.prodNA('iris.na', TRUE, NULL, x ='iris' )
+  expect_false(all(complete.cases(session1$iris.na)))
+  ds.missForest('iris_new', TRUE, NULL, xmis = 'session1$iris.na' )
+  expect_true(all(complete.cases(session1$iris_new)))
 })
 
