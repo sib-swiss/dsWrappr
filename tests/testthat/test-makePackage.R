dfaa9608c2b08c910224a46c5cea0045f23fa5d6
Iulian Dragan <iulian_dragan@yahoo.com>
2025-05-21 11:29:40 +0200

    test 1 passed

--- tests/testthat/test-makePackage.R
+++ tests/testthat/test-makePackage.R
@@ -1,8 +1,15 @@
-test_that("I can create the package dsMissForest", {
+test_that("I can create and load dsMissForest/dsMissForestClient", {
+  dPath = paste0(tempdir(TRUE), '/test_dsWrapper')
+  unlink(dPath, recursive = TRUE)
+  dir.create(dPath)
   makePackage('dsMissForest', assignList = list(missForest = c('missForest', 'prodNA')),
               aggregateList = list(missForest = c('mixError', 'nrmse')),
               symbols = list('ximp', 'xmiss', 'xtrue', 'x'),
               authors = 'person("Iulian", "Dragan", email = "iulian.dragan@sib.swiss", role = c("aut", "cre"))',
-              license = 'GPLv3', destPath = '/mnt/shareddisk/datashield')
-    #expect_equal(x$server1$Sepal.Length, c('low', 'high'))
+              license = 'GPLv3', destPath = dPath)
+  devtools::load_all(paste0(dPath, '/dsMissForestClient'))
+  devtools::load_all(paste0(dPath, '/dsMissForest'))
+  x <-sessionInfo(package = c('dsMissForest', 'dsMissForestClient'))
+  expect_equal(names(x$otherPkgs), c('dsMissForest', 'dsMissForestClient'))
+
 })
