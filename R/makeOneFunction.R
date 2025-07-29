60a718c3e7ab80b1325bcafe9c989e38f3300975
Iulian Dragan <iulian_dragan@yahoo.com>
2025-07-29 16:28:58 +0200

    bugfix

--- R/makeOneFunction.R
+++ R/makeOneFunction.R
@@ -5,24 +5,25 @@ makeOneFunction <- function(package, funcName, funcType = c('aggregate','assign'
     stop('funcType must be one of "aggregate" and "assign"')
   }
   serverFuncName <- paste0(funcName, serverSuffix)
-  clientFuncText <- readLines(system.file('client', 'function_template.md', package='dsWrapR'))
+  clientFuncText <- paste(readLines(system.file('client', 'function_template.md', package='dsWrapR')), collapse = "\n")
   if(funcType == 'assign' ){
     if(is.null(assignVar)){
       assignVar <- 'newObj'
     }
   }
   dict <- list(assign = c("@assignVar" = assignVar, "@serverFunction" = serverFuncName, "@op" = "assign", "@retVal" = assignVar),
-               aggregate = c("@assignVar," = NULL, "@serverFunction" = serverFuncName, "@op" = "aggregate", "@retVal" = "ret"))
+               aggregate = c("@assignVar," = "", "@serverFunction" = serverFuncName, "@op" = "aggregate", "@retVal" = "ret"))
 
   func$client <- stringr::str_replace_all(clientFuncText, dict[[funcType]])
 
 
   ### server
-  serverFuncText <- readLines(system.file('server', 'function_template.md', package='dsWrapR'))
+  serverFuncText <- paste(readLines(system.file('server', 'function_template.md', package='dsWrapR')), collapse = "\n")
+  printSyms <- ""
   if(length(symbols > 0 )){
-    printSyms <- paste(symbols, collapse = "', '")
+    printSyms <- paste0("'",paste(symbols, collapse = "', '"),"'")
   }
-  func$server <- stringr::str_replace_all(serverFuncText, c("@symbolList" = printSyms, "@packageName" = package, "@funcName" = funcName))
+  func$server <- stringr::str_replace_all(serverFuncText, c("@symbolList" = printSyms, "@packageName" = package, "@functionName" = funcName))
 
   return(func)
 }
