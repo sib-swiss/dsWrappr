bb7eb5ee8ebadcb2eeed6c8f3cc30be92d93dff3
Iulian Dragan <iulian_dragan@yahoo.com>
2025-09-03 13:52:37 +0200

    setup-init client

--- inst/client/setup-init.R
+++ inst/client/setup-init.R
@@ -1,9 +1,9 @@
 library(DSLite)
 library(dsBase)
 library(dsBaseClient)
-#pkgload::load_all('<server_package_path>')
-#dslite.server1 <- newDSLiteServer(config = defaultDSConfiguration(include=c('dsBase', '<server_package>')))
-#dslite.server2 <- newDSLiteServer(config = defaultDSConfiguration(include=c('dsBase', '<server_package>')))
+pkgload::load_all('<server_package_path>')
+dslite.server1 <- newDSLiteServer(config = defaultDSConfiguration(include=c('dsBase', '<server_package>')))
+dslite.server2 <- newDSLiteServer(config = defaultDSConfiguration(include=c('dsBase', '<server_package>')))
 
 #library(DSI)
 #library(dsBaseClient)
@@ -17,8 +17,8 @@ builder$append(server="server2", url='dslite.server2',driver = "DSLiteDriver")
 logindata <- builder$build()
 
 
-opals <- datashield.login(logins = logindata)
-session1 <- dslite.server1$getSession(dslite.server1$getSessionIds())
-session2 <- dslite.server2$getSession(dslite.server2$getSessionIds())
+opals <<- datashield.login(logins = logindata)
+session1 <<- dslite.server1$getSession(dslite.server1$getSessionIds())
+session2 <<- dslite.server2$getSession(dslite.server2$getSessionIds())
 data('iris', envir = session1)
 
